49a1b8646200c23071190779383645a8da49d766
unknown <afif.azizy@gmail.com>
2024-09-06 17:31:51 +0700

    initial

---
+++ entities.js
@@ -0,0 +1,64 @@
+class SystemEntity {
+    /**
+     * 
+     * @param {{ type: 'CONN' | 'SSH', mysql_config: MySQLConfig, ssh_config: SSHConfig, system_name: string }} object 
+     */
+    constructor(object = { type: null, mysql_config: null, ssh_config: null, system_name: null }) {
+        try {
+            if (object.system_name == null || object.type == null) { throw new Error("Null object property") }
+            if (object.mysql_config == null) { throw new Error("Null object property") }
+
+            this.mysql_config = object.mysql_config
+            this.system_name = object.system_name
+            this.type = object.type
+            if (object.type == 'SSH') {
+                if (object.ssh_config == null) { throw new Error("Null object property") }
+                this.ssh_config = object.ssh_config
+            }
+        } catch (error) {
+            console.log("Invalid config on one or more system!", error.message)
+            process.exit()
+        }
+    }
+}
+
+class SSHConfig {
+    /**
+     * 
+     * @param {{ ssh_key_path: string, mysqldump_path: string, ssh_host: string, port: number }} object 
+     */
+    constructor(object = { ssh_key_path: null, mysqldump_path: null, ssh_host: null, port: null }) {
+        try {
+            if (object.mysqldump_path == null || object.ssh_host == null || object.ssh_key_path == null) { throw new Error("Null object property") }
+            this.mysqldump_path = object.mysqldump_path
+            this.ssh_key_path = object.ssh_key_path
+            this.ssh_host = object.ssh_host
+            this.port = object.port
+        } catch (error) {
+            console.log("Invalid config on one or more system!")
+            process.exit()
+        }
+    }
+}
+
+class MySQLConfig {
+    /**
+     * 
+     * @param {{ user: string, password: string, database: string, host: string, port: number }} object 
+     */
+    constructor(object = { user: null, password: null, database: null, host: null, port: null }) {
+        try {
+            if (object.user == null, object.password == null, object.database == null, object.host == null, object.port == null) { throw new Error("Null object property") }
+            this.user = object.user
+            this.password = object.password
+            this.database = object.database
+            this.host = object.host
+            this.port = object.port
+        } catch (error) {
+            console.log("Invalid config on one or more system!", error.message)
+            process.exit()
+        }
+    }
+}
+
+module.exports = { SystemEntity, MySQLConfig, SSHConfig }
---
+++ index.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+const { system_configs } = require('./config');
+const { SystemEntity, MySQLConfig, SSHConfig } = require('./entities')
+
+const read_args = () => {
+    /**
+     * @type {SystemEntity[]}
+     */
+    let process_db = []
+    const read = process.argv;
+    if (read[2][0] == '-') {
+        const sliced = read[2].slice(1)
+        switch (sliced) {
+            case 'all': {
+                process_db = system_configs
+                break;
+            }
+            case 'only': {
+                for (let i = 3; i < read.length; i++) {
+                    process_db.push(system_configs.find((val) => {
+                        return val.system_name == read[i]
+                    }))
+                }
+                break;
+            }
+            default:
+                console.log("Arguments unknown!")
+                process.exit()
+        }
+    } else {
+        console.log("Arguments needed!")
+        process.exit()
+    }
+
+    return process_db
+}
+
+/**
+ * 
+ * @param {SSHConfig} ssh_config 
+ * @param {MySQLConfig} mysql_config 
+ * @param {string} dir
+ * @returns {Promise<void>}
+ */
+const backup = async (ssh_config, mysql_config, dir) => {
+    const date = new Date();
+    const datetime = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+
+    let cmd = ``
+    cmd += `ssh -i ${ssh_config.ssh_key_path} ${ssh_config.ssh_host} ${ssh_config.port == null ? '' : `-p ${ssh_config.port}`} `
+    cmd += `"mysqldump -P ${mysql_config.port} -u ${mysql_config.user} -p${mysql_config.password} ${mysql_config.database} --no-tablespaces --single-transaction" `
+
+    const dump = child_process.exec(cmd)
+    const wstream = fs.createWriteStream(path.join(dir, `bak_${datetime}.sql`));
+
+    return new Promise((resolve, reject) => {
+        // we pipe manual
+        let written = 0;
+        dump.stdout.on('data', (data) => {
+            wstream.write(data, () => {
+                written += data.length;
+                console.log(`\x1B[2J\x1B[H`) // clear console
+                console.log(`Schema ${mysql_config.database} processed: ${written}`);
+            });
+        })
+        dump.stdout.on('error', (error) => {
+            reject(`Error!: ${error}`);
+        });
+        dump.stdout.on('finish', () => {
+            resolve();
+        });
+    })
+}
+
+/**
+ * 
+ * @param {SystemEntity[]} systems 
+ * @returns {Promise<void>}
+ */
+const start_backup = async (systems) => {
+    const generated = []
+    for (let idx = 0; idx < systems.length; idx++) {
+        const system = systems[idx]
+
+        if (system.type == 'SSH') {
+            if (!fs.existsSync(path.join(__dirname, 'backup', system.system_name))) {
+                fs.mkdirSync(path.join(__dirname, 'backup', system.system_name), { recursive: true })
+            }
+
+            try {
+                await backup(
+                    system.ssh_config,
+                    system.mysql_config,
+                    path.join(__dirname, 'backup', system.system_name)
+                );
+                generated.push(system.system_name);
+            } catch (error) {
+                console.log(`Failed to generate backup for: ${system.system_name}`, error);
+            }
+        }
+    }
+
+    console.log(generated)
+    process.exit();
+}
+
+start_backup(read_args())
